83280a242d0c67da188578e655e7484c823fca4f
rensilva5 <renzoasilva@gmail.com>
2023-12-17 15:10:44 -0500

    Index.js code

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const { onRequest } = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 const functions = require("firebase-functions");
 
@@ -20,9 +20,24 @@ const functions = require("firebase-functions");
 //   response.send("Hello from Firebase!");
 // });
 exports.createChatEngineUser = functions.auth.user().onCreate((user) => {
-  console.log("create", user);
+  axios.post(
+    "https://api.chatengine.io/users/",
+    {
+      username: user.email,
+      secret: user.uid,
+      email: user.email,
+      first_name: user.displayName,
+    },
+    { headers: { "Private-Key": "XXX" } }
+  );
 });
 
 exports.deleteChatEngineUser = functions.auth.user().onDelete((user) => {
-  console.log("delete", user);
+  axios.delete("https://api.chatengine.io/users/me/", {
+    headers: {
+      "Project-ID": "YYY",
+      "User-Name": user.email,
+      "User-Secret": user.uid,
+    },
+  });
 });
